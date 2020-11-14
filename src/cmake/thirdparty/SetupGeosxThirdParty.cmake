249bffa25d69786bc43a4a238a4cbca5d4645614
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-11-13 16:33:03 -0800

    More travis.

--- src/cmake/thirdparty/SetupGeosxThirdParty.cmake
+++ src/cmake/thirdparty/SetupGeosxThirdParty.cmake
@@ -220,7 +220,7 @@ if(DEFINED CHAI_DIR)
 
     get_target_property(CHAI_INCLUDE_DIRS chai INTERFACE_INCLUDE_DIRECTORIES)
     set_target_properties(chai
-                        PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${CHAI_INCLUDE_DIRS}")
+                          PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${CHAI_INCLUDE_DIRS}")
 
     set(ENABLE_CHAI ON CACHE BOOL "" FORCE)
     set(thirdPartyLibs ${thirdPartyLibs} chai)
